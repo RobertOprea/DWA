88e5b668f3eecc8626707fb6118e022b6c122d3a
Robert Oprea <robert.oprea@corp.ppbplc.com>
2018-04-17 23:42:51 +1000

    Integrate Butterknife into application

--- app/src/main/java/com/dwa/rybridge/ryebridgedwa/ui/activities/LoginActivity.java
+++ app/src/main/java/com/dwa/rybridge/ryebridgedwa/ui/activities/LoginActivity.java
@@ -3,8 +3,12 @@ package com.dwa.rybridge.ryebridgedwa.ui.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.dwa.rybridge.ryebridgedwa.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,5 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ButterKnife.bind(this);
     }
 }
